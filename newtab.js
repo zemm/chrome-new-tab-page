407b9eb586c9de7b88070048eabd7678d1028394
Jussi Rajala <zemm@iki.fi>
2020-01-03 22:27:04 +0200

    Remove transitions, try to get the gradient to be more balanced

--- newtab.js
+++ newtab.js
@@ -1,14 +1,12 @@
-
-function set_random_hue() {
-	console.log('hueing');
-	var el = document.getElementById("content");
-	if (el) {
-		console.log('element', el);
-		window.foo = el;
-		var hue = Math.round(Math.random() * 360);
-		el.style.filter = "hue-rotate(" + hue + "deg)";
-		el.style.opacity = 0.7;
-	}
+function rndi(max) {
+	return Math.round(Math.random() * max);
 }
 
-set_random_hue();
+function set_random_hue(el) {
+	var hue = rndi(360);
+	//document.title = "(" + hue + ")";
+	el.style.filter = "hue-rotate(" + hue + "deg)";
+}
+
+//set_random_hue(document.getElementById("content"));
+set_random_hue(document.body);
